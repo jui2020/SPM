40ccee68758ea340eb2a48fa974c7e8d864ae2c9
JUI MISTRY <73281183+jui2020@users.noreply.github.com>
2023-02-10 15:28:08 +0530

    added task-5

--- All task in one/src/components/Todo.js
+++ All task in one/src/components/Todo.js
@@ -1,129 +1,121 @@
-import React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import '../components/Todo.css'
 
-const Todo = () => {
-  const [task, updateTask] = useState("");
-  const [toDoList, upadateToDoList] = useState([]);
+export default function Todo() {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
 
-  // add task to the List
+  const [todo, setTodo] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({});
 
-  const addTask = () => {
-    if (!task || !task.length)
-    return;
-    toDoList.push(getTaskObject(task,false));
-    upadateToDoList(toDoList);
-    updateTask("");
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  //Add functionality
+  const handleInputChange = (e) => {
+    setTodo(e.target.value);
   };
 
-  // was created heating and enter and adding to the list 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
-  const inputKeyDown = (event) => {
-    if(event.keycode===13)
-    addTask();
-  } 
+    if (todo !== "") {
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: todo.trim(),
+        },
+      ]);
+    }
 
-
-const getTaskObject =(description , iscomplete) => {
-  return{
-    description,
-    iscomplete
-  }
-}
-
-//to delete the task 
-  const deleteTask = (index) => {
-    const newList = toDoList.filter((item, i) => i !== index);
-    upadateToDoList(newList);
+    setTodo("");
   };
 
-
-  //to make it mark as done and update 
-  const markComplete = (index) => {
-    const list = [...toDoList];
-    list[index].isComplete = !list[index].isComplete;
-    upadateToDoList(list);
+  // Delete functionality
+  const handleDeleteClick = (id) => {
+    const removeItem = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(removeItem);
   };
 
-  
+  // function to edit a todo item
+  const handleUpdateTodo = (id, updatedTodo) => {
+    const updatedItem = todos.map((todo) => {
+      return todo.id === id ? updatedTodo : todo;
+    });
+    setIsEditing(false);
+    setTodos(updatedItem);
+  };
+
+  const handleEditFormSubmit = (e) => {
+    e.preventDefault();
+    handleUpdateTodo(currentTodo.id, currentTodo);
+  };
+
+  const handleEditInputChange = (e) => {
+    setCurrentTodo({ ...currentTodo, text: e.target.value });
+    console.log(currentTodo);
+  };
+
+  // function to handle when the "Edit" button is clicked
+  const handleEditClick = (todo) => {
+    // set editing to true
+    setIsEditing(true);
+    // set the currentTodo to the todo item that was clicked
+    setCurrentTodo({ ...todo });
+  };
+
   return (
-    <>
-      <div className="app-background">
-        <p className="heading-text">Todo Using Hook UseSate</p>
-        <div className="task-container">
-          <div className="input-button">
-            <input
-              className="text-input"
-              value={task} 
-              onKeyDown={inputKeyDown}
-              onChange={(event) => updateTask(event.target.value)}
-            />
-            <button className="add-button" onClick={addTask}>
-              +
-            </button>
+    <div className="todocontainer">
+      {isEditing ? (
+        <form onSubmit={handleEditFormSubmit}>
+          <h2>Edit Todo</h2>
+          <label htmlFor="editTodo">Edit todo: </label>
+          <input
+            name="editTodo"
+            type="text"
+            placeholder="Edit todo"
+            value={currentTodo.text}
+            onChange={handleEditInputChange}
+          />
+          <button type="submit">Update</button>
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </form>
+      ) : (
+        <form onSubmit={handleFormSubmit}>
+          <h2>Add Todo</h2>
+          <label htmlFor="todo">Add todo: </label>
+          <input
+            name="todo"
+            type="text"
+            placeholder="Create a new todo"
+            value={todo}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+      )}
+
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <div key={todo.id}>
+            <p>{index + 1 + "." + todo.text}</p>
+            {/* we are passing the entire todo object to the handleEditClick function*/}
+            <button onClick={() => handleEditClick(todo)}>Edit</button>
+            <button onClick={() => handleDeleteClick(todo.id)}>Delete</button>
           </div>
-          {toDoList?.length ? (
-            toDoList.map((toDoObject, index) => (
-              <ListItems
-                index={index}
-                itemData={toDoObject}
-                deleteTask={deleteTask}
-                markComplete={markComplete}
-              />
-            ))
-          ) : (
-            <p className="no-item-text">no task added!</p>
-          )}
-        </div>
-        <p className="footer">click on task to make it mark as done</p>
-      </div>
-    </>
+        ))}
+      </ul>
+    </div>
   );
-};
-
-
-//Submodule For listing the items
-
-const ListItems = (props) => {
-  return (
-    <>
-      <div className="list-item row jc-space-between">
-        <span 
-        className={props.iteData.isComplete?"task-complete":""}
-
-        // className={props.itemData.isComplete ? 'task-complete' : ''}
-                  onClick={() => props.markComplete(props.index)}>{props.itemData.isComplete ? `✅ ` : ''}
-        {() => props.markComplete(props.index)}
-          {props.itemData.description}
-        </span>
-        <button
-          className="delete-icon"
-          onClick={() => props.deleteTask(props.index)}>
-          Delete
-        </button>
-      </div>
-    </>
-  );
-
-
-};
-
-export default Todo;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
